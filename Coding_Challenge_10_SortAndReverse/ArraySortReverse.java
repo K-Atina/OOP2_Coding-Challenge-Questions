6f3ea3bf1bc73f22fc6d5e65646f1a33dcbfc0b0
Atina <kellieatina@gmail.com>
2025-06-24 10:22:53 +0300

    Add ArraySortReverse class to sort and reverse an array of pallet IDs

---
+++ Coding_Challenge_10_SortAndReverse/ArraySortReverse.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraySortReverse {
+    public static void main(String[] args) {
+        String[] pallets = {"B094","C983","D656","E510","A575"};
+        System.out.println("Sorted Array");
+        Arrays.sort(pallets);
+
+        for(String pallet : pallets){
+            System.out.println("--> " + pallet);
+        }
+
+        System.out.println("\nReversed Array");
+        List<String> palletList = Arrays.asList(pallets);
+        Collections.reverse(palletList);
+
+        for(String pallet : palletList){
+            System.out.println("--> " + pallet);
+        }
+    }
+}
